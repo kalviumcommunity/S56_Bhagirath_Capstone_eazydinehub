add61c44fa975e48236717f6e7b0894c403f1dfb
bhagirathauti <bhagirath961778@gmail.com>
2024-04-10 13:21:12 +0530

    login path created

--- server/routes.js
+++ server/routes.js
@@ -28,13 +28,39 @@ router.post("/signup", async (req, res) => {
     const newUser = await users.create({ name, email, password: hashedPassword });
     console.log(newUser);
     const token = jwt.sign(
-      { userId: newUser._id, email: newUser.email, password: newUser.password },
+      { userId: newUser._id, email: newUser.email },
       jwtSecret,
       { expiresIn: "1h" }
     );
     res.status(201).json({ message: "User created successfully", token });
   } catch (error) {
     console.error("Error creating user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await users.findOne({ email: email });
+    if (!user) {
+      return res.status(401).json({ error: "User not found" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Incorrect password" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      jwtSecret,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    console.error("Error logging in:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
