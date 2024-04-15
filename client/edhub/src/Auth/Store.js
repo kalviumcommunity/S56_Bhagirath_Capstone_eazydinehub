66d48d84b41fce4f97269b3c59802228fb4ec90f
bhagirathauti <bhagirath961778@gmail.com>
2024-04-15 13:31:34 +0530

    thunk error solved

--- client/edhub/src/Auth/Store.js
+++ client/edhub/src/Auth/Store.js
@@ -1,12 +1,12 @@
 import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
-import thunk from 'redux-thunk';
+import {thunk} from 'redux-thunk';
 import { rootReducer } from "./rootReducer";
 
 const store = configureStore({
     reducer: {
         root: rootReducer
     },
-    middleware: [thunk]
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 });
 
 export default store;
