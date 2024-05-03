cbcf623e1a2d2cb9335e582bc58fd95612cf628e
bhagirathauti <bhagirath961778@gmail.com>
2024-05-03 10:48:54 +0530

    auth0 authentication added

--- client/edhub/src/Pages/Customer/CustomerProfile.jsx
+++ client/edhub/src/Pages/Customer/CustomerProfile.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import Navbar from '../Components/Navbar.jsx';
-import { useDispatch } from 'react-redux';
-import {logout} from "../../actions.js"
 import { useNavigate } from 'react-router-dom';
 function CustomerProfile() {
-  const dispatch = useDispatch();
   const navigate = useNavigate()
   const handleLogout = () => {
-    dispatch(logout());
     localStorage.clear()
     navigate("/landingpage")
   };
--- client/edhub/src/main.jsx
+++ client/edhub/src/main.jsx
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Provider} from "react-redux"
-import store from './Auth/Store.js';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+const landingpage = "http://localhost:5173/landingpage"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
+<Auth0Provider
+    domain="dev-cc0145p768m2id31.us.auth0.com"
+    clientId="wPpxlMccTJdBw4uLzTzSoo8lApAp0TKk"
+    authorizationParams={{
+      redirect_uri: landingpage
+    }}
+  >
     <App />
     <ToastContainer position='top-center' style={{width:"300px"}}/>
-    </Provider>
-  </React.StrictMode>,
+  </Auth0Provider>,
+</React.StrictMode>
 )
