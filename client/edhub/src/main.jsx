4e530f3325b5470fb3cd5b0b642161cc957ce9f3
bhagirathauti <bhagirath961778@gmail.com>
2024-05-03 11:51:57 +0530

    .

--- client/edhub/src/main.jsx
+++ client/edhub/src/main.jsx
@@ -4,14 +4,14 @@ import App from './App.jsx'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Auth0Provider } from '@auth0/auth0-react';
-require('dotenv').config();
+const {VITE_domain,VITE_cliendid} = import.meta.env
 
 const landingpage = "https://eazydinehub.netlify.app/landingpage"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 <Auth0Provider
-    domain={process.env.domain}
-    clientId={process.env.clientid}
+    domain={VITE_domain}
+    clientId={VITE_cliendid}
     authorizationParams={{
       redirect_uri: landingpage
     }}
